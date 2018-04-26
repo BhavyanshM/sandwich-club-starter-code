8a211d2d274d784d366bea0503bcc87d3d76e618
BhavyanshM <bhavyanshmishra@gmail.com>
2018-04-25 22:22:49 -0500

    Add the code to populate a sandwich object using JSON data from the strings.xml

--- app/src/main/java/com/udacity/sandwichclub/utils/JsonUtils.java
+++ app/src/main/java/com/udacity/sandwichclub/utils/JsonUtils.java
@@ -2,9 +2,47 @@ package com.udacity.sandwichclub.utils;
 
 import com.udacity.sandwichclub.model.Sandwich;
 
-public class JsonUtils {
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+
+public class JsonUtils{
 
     public static Sandwich parseSandwichJson(String json) {
+        String mainName;
+        String placeOfOrigin;
+        String description;
+        String image;
+        ArrayList<String> alsoKnownAs;
+        ArrayList<String> ingredients;
+        try {
+            JSONObject fullObject = new JSONObject(json);
+            JSONObject nameObject = fullObject.getJSONObject("name");
+            mainName = nameObject.getString("mainName");
+            JSONArray alsoKnownAsJSONArray = nameObject.getJSONArray("alsoKnownAs");
+            placeOfOrigin = fullObject.getString("placeOfOrigin");
+            description = fullObject.getString("description");
+            image = fullObject.getString("image");
+            JSONArray ingredientsJSONArray = fullObject.getJSONArray("ingredients");
+
+            alsoKnownAs = new ArrayList<>();
+            for(int i = 0; i<alsoKnownAsJSONArray.length(); i++){
+                alsoKnownAs.add(alsoKnownAsJSONArray.getString(i));
+            }
+
+            ingredients = new ArrayList<>();
+            for(int i = 0; i<ingredientsJSONArray.length(); i++){
+                ingredients.add(ingredientsJSONArray.getString(i));
+            }
+
+            Sandwich sandwich = new Sandwich(mainName, alsoKnownAs, placeOfOrigin, description, image, ingredients);
+            return sandwich;
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+
         return null;
     }
 }
